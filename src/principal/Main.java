8fa5ff7eec4a03f3567d53145346c26e4fa5b2c3
nunescostta <jeanrodrigo1324@gmail.com>
2024-03-05 14:43:51 -0300

    Alterado classe Main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,15 +1,40 @@
 package principal;
 
+import java.util.Scanner;
+
 import controller.AlunoController;
+import repositories.AlunoRepository;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		AlunoController alunoController = new AlunoController();
+		
+		System.out.println("(1) - CADASTRAR ALUNO");
+		System.out.println("(2) - ATUALIZAR ALUNO");
+		System.out.println("(3) - EXCLUIR ALUNO");
+		System.out.println("(4) - CONSULTAR ALUNO");
 
-		alunoController.cadastroAluno();
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe a Opção Desejada.....:");
+		Integer opcao = Integer.parseInt(scanner.nextLine());
+		
+		switch (opcao) {
+		case 1:
+			alunoController.cadastroAluno();
+		case 2:
+			alunoController.atualizarAluno();
+		case 3:
+			alunoController.excluirAluno();
+		case 4:
+			alunoController.consultarAluno();
+		default:
+			System.out.println("OPÇÃO INVALIDA");
+			break;
+		}
+		scanner.close();
 	}
 
 }
